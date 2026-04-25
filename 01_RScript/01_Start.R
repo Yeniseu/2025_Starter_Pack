522795a9181e42bb667adbe2db189aef83456d24
Orhun <orhun1337@gmail.com>
2026-04-25 18:59:53 +0200

    Script

---
+++ 01_RScript/01_Start.R
@@ -0,0 +1,13 @@
+# Encoding: UTF-8
+# 2025_08_18
+# Orhun Ozel
+# Creates Example for Creating Price Indices
+# Intented to lorem ipsum
+
+library(data.table)
+library(ggplot2)
+
+rm(list=ls())
+options(scipen=40, digits=4)
+options(datatable.print.trunc.cols = T)
+options(datatable.print.nrows      = 15)
